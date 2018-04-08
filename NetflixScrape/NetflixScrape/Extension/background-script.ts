941f9729e534b54ddd57cf396c38b86f81814884
Jeremy@slartybartfast <jeremy.blamey@gmail.com>
2018-04-08 15:28:53 +1000

    Proof-of-concept web-extension: signals on location change, hash change, and page load

---
+++ NetflixScrape/NetflixScrape/Extension/background-script.ts
@@ -0,0 +1,15 @@
+﻿namespace JBlam.NetflixScrape.Extension {
+    
+    browser.webNavigation.onHistoryStateUpdated.addListener(async evt => {
+        console.log("history state updated");
+        let tabs = await browser.tabs.query({ currentWindow: true, active: true });
+        for (let t of tabs) {
+            await browser.tabs.sendMessage(t.id, `onHistoryStateUpdated: ${evt.url}`);
+            console.log("messaged tab", t.id);
+        }
+    });
+
+    browser.runtime.onMessage.addListener(message => {
+        console.log("background received message:", message);
+    });
+}
---
+++ NetflixScrape/NetflixScrape/Extension/content-script.ts
@@ -0,0 +1,27 @@
+﻿namespace JBlam.NetflixScrape.Extension {
+    const feedbackElement = document.createElement('div');
+    feedbackElement.style.position = 'absolute';
+    feedbackElement.style.top = '10px';
+    feedbackElement.style.left = '10px';
+    feedbackElement.style.minWidth = '33%';
+    feedbackElement.style.maxWidth = '66%';
+    feedbackElement.style.opacity = '0.25';
+    feedbackElement.style.border = 'solid 2px black';
+    feedbackElement.style.background = 'white';
+    feedbackElement.style.color = 'red';
+    document.body.appendChild(feedbackElement);
+    feedbackElement.textContent = "Hi, this is a script";
+    console.log("This is a script");
+
+    browser.runtime.onMessage.addListener(message => {
+        console.log("received message:", message);
+        feedbackElement.textContent = message;
+    });
+
+    browser.runtime.sendMessage(`page loaded: ${location}`);
+
+    window.addEventListener('hashchange', async evt => {
+        await browser.runtime.sendMessage(`hash changed: ${evt.newURL}`);
+        console.log("sent hashchange", evt);
+    })
+}
